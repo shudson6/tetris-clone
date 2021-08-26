bd74212cfd35ab85a6f0d40f39c4ee55e5105122
Steven Hudson <shudson6@asu.edu>
2021-08-26 18:13:32 -0400

    figured out how to make the tetrominos

--- tetris.js
+++ tetris.js
@@ -18,21 +18,6 @@ class Tetromino {
     this.blocks = [ a, b, c, d ];
   }
 
-  /**
-   * Rotates this Tetromino. The standard direction is clockwise, but
-   * if counterclockwise === true it will rotate that way.
-   * 
-   * @param {*} counterclockwise indicates if rotation should be reversed
-   */
-  rotate(counterclockwise) {
-    const sinTheta = counterclockwise ? 1 : -1;
-    for (let v of this.blocks) {
-      const [x, y] = v;
-      v[0] = -y * sinTheta;
-      v[1] = x * sinTheta;
-    }
-  }
-
   drawSelf(context, x, y) {
     for (const [i, j] of this.blocks) {
       drawBlock(context, i + x, j + y);
@@ -49,34 +34,86 @@ class Tetromino {
     ];
   }
 
-  static L() {
-    return new Tetromino([0,0], [1,0], [2,0], [2,1]);
-  } 
+  /**
+   * Creates a doubly-linked circular list from the parameters. If only one is 
+   * given, it _will_ link to itself in both directions.
+   * 
+   * Intented for creating the static tetrominos below.
+   * 
+   * @param  {...any} rotations 
+   * @returns 
+   */
+  static createLinkedRotation(...rotations) {
+    function link(l) {
+      return () => { return l; };
+    }
 
-  static J() {
-    return new Tetromino([0,0], [0,1], [1,1], [2,1]);
+    for (let i = 1; i < rotations.length; i++) {
+      rotations[i - 1].nextRotation = link( rotations[i] );
+      rotations[i].prevRotation = link( rotations[i - 1] );
+    }
+    const first = rotations[0];
+    const last = rotations[rotations.length - 1];
+    last.nextRotation = link( first );
+    first.prevRotation = link( last );
+
+    for (const r of rotations) {
+      Object.freeze( r );
+    }
+
+    return first;
   }
 
-  static S() {
-    return new Tetromino([1,0], [2,0], [0,1], [1,1]);
-  }
+  // rotations from the Super Rotation System 
+  // described at https://strategywiki.org/wiki/Tetris/Rotation_systems
+  static I = Tetromino.createLinkedRotation(
+    new Tetromino([0,0], [1,0], [2,0], [3,0]),
+    new Tetromino([2,-1], [2,0], [2,1], [2,2]),
+    new Tetromino([0,1], [1,1], [2,1], [3,1]),
+    new Tetromino([1,-1], [1,0], [1,1], [1,2])
+  );
 
-  static Z() {
-    return new Tetromino([0,0], [1,0], [1,1], [2,1]);
-  } 
+  static J = Tetromino.createLinkedRotation(
+    new Tetromino([0,0], [0,1], [1,1], [2,1]),
+    new Tetromino([1,0], [1,1], [1,2], [2,0]),
+    new Tetromino([0,1], [1,1], [2,1], [2,2]),
+    new Tetromino([0,2], [1,0], [1,1], [1,2])
+  );
 
-  static I() {
-    return new Tetromino([0,0], [1,0], [2,0], [3,0]);
-  } 
+  static L = Tetromino.createLinkedRotation(
+    new Tetromino([0,1], [1,1], [2,0], [2,1]),
+    new Tetromino([1,0], [1,1], [1,2], [2,2]),
+    new Tetromino([0,1], [0,2], [1,1], [2,1]),
+    new Tetromino([0,0], [1,0], [1,1], [1,2])
+  );
 
-  static T() {
-    return new Tetromino([1,0], [0,1], [1,1], [2,1]);
-  } 
+  static O = Tetromino.createLinkedRotation(
+    new Tetromino([0,0], [0,1], [1,0], [1,1])
+  ); 
 
-  static O() {
-    return new Tetromino([0,0], [0,1], [1,0], [1,1]);
-  } 
+  static S = Tetromino.createLinkedRotation(
+    new Tetromino([0,1], [1,0], [1,1], [2,0]),
+    new Tetromino([1,0], [1,1], [2,1], [2,2]),
+    new Tetromino([0,2], [1,1], [1,2], [2,1]),
+    new Tetromino([0,0], [0,1], [1,1], [1,2])
+  );
+
+  static T = Tetromino.createLinkedRotation(
+    new Tetromino([0,1], [1,0], [1,1], [2,1]),
+    new Tetromino([1,0], [1,1], [1,2], [2,1]),
+    new Tetromino([0,1], [1,1], [1,2], [2,1]),
+    new Tetromino([0,1], [1,0], [1,1], [1,2])
+  );
+
+  static Z = Tetromino.createLinkedRotation(
+    new Tetromino([0,0], [1,0], [1,1], [2,1]),
+    new Tetromino([1,1], [1,2], [2,0], [2,1]),
+    new Tetromino([0,1], [1,1], [1,2], [2,2]),
+    new Tetromino([0,1], [0,2], [1,0], [1,1])
+  );
 }
+
+
 
 function drawBlock(context, x, y) {
   context.beginPath();
@@ -90,11 +127,32 @@ function drawBlock(context, x, y) {
 
 const ctx = document.getElementById("playfield").getContext("2d");
 
-let test = Tetromino.L();
+let test = [
+  Tetromino.I,
+  Tetromino.J,
+  Tetromino.L,
+  Tetromino.O,
+  Tetromino.S,
+  Tetromino.T,
+  Tetromino.Z
+];
 
-test.drawSelf(ctx, 3, 7);
+test[0].drawSelf(ctx, 1, 1);
+test[1].drawSelf(ctx, 1, 5);
+test[2].drawSelf(ctx, 1, 9);
+test[3].drawSelf(ctx, 1, 13);
+test[4].drawSelf(ctx, 6, 3);
+test[5].drawSelf(ctx, 6, 7);
+test[6].drawSelf(ctx, 6, 11);
+
 setInterval(() => {
   ctx.clearRect(0, 0, 480, 640);
-  test.rotate();
-  test.drawSelf(ctx, 6, 7);
+  test = test.map(t => t.nextRotation());
+  test[0].drawSelf(ctx, 1, 1);
+  test[1].drawSelf(ctx, 1, 5);
+  test[2].drawSelf(ctx, 1, 9);
+  test[3].drawSelf(ctx, 1, 13);
+  test[4].drawSelf(ctx, 6, 3);
+  test[5].drawSelf(ctx, 6, 7);
+  test[6].drawSelf(ctx, 6, 11);
 }, 500);
